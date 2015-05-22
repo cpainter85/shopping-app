fdbc3ec79b42dd74ab90414800d00d6fb176a7d7
Andrew Deitrick <ajdeitrick@gmail.com>
2015-05-22 13:40:15 -0600

    update user spec

--- spec/features/user_flow_spec.rb
+++ spec/features/user_flow_spec.rb
@@ -5,47 +5,47 @@ feature "as a user" do
 		visit root_path
 		expect(page).to have_link "Sign In"
 		expect(page).to_not have_link "Sign Out"
-		expect(page).to_not have_content "Logged in as Sean Connery"
+		expect(page).to_not have_content "Logged in as Sample User"
 		expect(page).to_not have_content "Thanks for signing up!"
 
 		click_on "Sign Up"
 		expect(current_path).to eq(new_user_path)
 
-		fill_in :username, with: "Sean Connery"
-		fill_in :password, with: "connery"
-		fill_in :password_confirmation, with: "connery"
+		fill_in "Username", with: "Sample User"
+		fill_in "Password", with: "password"
 
-		click_on "Sign Up"
+		click_button "Sign Up"
 		expect(current_path).to eq(root_path)
 
 		expect(page).to have_content "Thanks for signing up!"
-		expect(page).to have_content "Logged in as Sean Connery"
+		expect(page).to have_content "Logged in as Sample User"
 		expect(page).to_not have_link "Sign In"
 		expect(page).to_not have_link "Sign Up"
 
 		visit current_path
-		expect(page).to have_content "Logged in as Sean Connery"
+		expect(page).to have_content "Logged in as Sample User"
 		expect(page).to_not have_content "Thanks for signing up!"
 
 		click_on "Sign Out"
 
 		expect(page).to have_content "Signed out!"
-		expect(page).to_not have_content "Logged in as Sean Connery"
+		expect(page).to_not have_content "Logged in as Sample User"
 
 		click_on "Sign In"
 
-		fill_in :username, with: "Sean Connery"
-		fill_in :password, with: "connery"
+		fill_in "Username", with: "Sample User"
+		fill_in "Password", with: "password"
 
-		click_on "Sign In"
+		click_button "Sign In"
+		expect(current_path).to eq root_path
 
 		expect(page).to have_content "Thanks for signing in!"
-		expect(page).to have_content "Logged in as Sean Connery"
+		expect(page).to have_content "Logged in as Sample User"
 		expect(page).to_not have_link "Sign In"
 		expect(page).to_not have_link "Sign Up"
 
 		visit current_path
-		expect(page).to have_content "Logged in as Sean Connery"
+		expect(page).to have_content "Logged in as Sample User"
 		expect(page).to_not have_content "Thanks for signing in!"
 	end
 end
